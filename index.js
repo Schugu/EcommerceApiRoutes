3643bb2f2df6cae6c4d2cd3190c7c095e0c3c5f4
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-08-15 23:27:19 -0300

    Add: Variable de entorno para puertos

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import app from "./src/app.js"
 
-const PORT = 8080;
+const PORT = process.env.PORT ?? 8080;
 
 app.listen(PORT, () => {
   console.log(`Server abierto en: ${PORT}`);
